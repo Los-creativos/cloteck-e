1b25308c1207fd2431a4f51b97d725c975ef5854
JosuePrado <prado69508698@gmail.com>
2023-11-09 22:52:02 -0400

    implement category crud, update data base configuration and implement prisma conection

--- app/DataBase/sql/init.sql
+++ app/DataBase/sql/init.sql
@@ -1,84 +1,105 @@
 SET TIME  ZONE 'UTC';
 
-CREATE TABLE IF NOT EXISTS "Customer" (
-    customer_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    phone_number INTEGER,
-    type_user VARCHAR(50) NOT NULL
+CREATE TABLE "Customer" (
+    "customer_id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "phone_number" INTEGER,
+    "type_user" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("customer_id")
 );
 
-CREATE TABLE IF NOT EXISTS "Product" (
-   product_id SERIAL PRIMARY KEY,
-   name VARCHAR(50) NOT NULL,
-   description VARCHAR(200) NOT NULL,
-   price INT NOT NULL,
-   image CHAR(100),
-   quantity INT NOT NULL
+CREATE TABLE "Product" (
+    "product_id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "description" VARCHAR(200) NOT NULL,
+    "price" INTEGER NOT NULL,
+    "image" CHAR(100),
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("product_id")
 );
 
-CREATE TABLE IF NOT EXISTS "Category" (
-   category_id SERIAL PRIMARY KEY,
-   name VARCHAR(50) NOT NULL,
-   description VARCHAR(200) NOT NULL
+CREATE TABLE "Category" (
+    "category_id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "description" VARCHAR(200) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("category_id")
 );
 
-CREATE TABLE IF NOT EXISTS "ProductCategory" (
-    product_category_id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,
-    category_id INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES "Product"(product_id),
-    FOREIGN KEY (category_id) REFERENCES "Category"(category_id)
+CREATE TABLE "ProductCategory" (
+    "product_category_id" SERIAL NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "category_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductCategory_pkey" PRIMARY KEY ("product_category_id")
 );
 
-CREATE TABLE IF NOT EXISTS "Size" (
-    size_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+CREATE TABLE "Size" (
+    "size_id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "Size_pkey" PRIMARY KEY ("size_id")
 );
 
-CREATE TABLE IF NOT EXISTS "ProductSize" (
-    product_size_id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,
-    size_id INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES "Product"(product_id),
-    FOREIGN KEY (size_id) REFERENCES "Size"(size_id)
+CREATE TABLE "ProductSize" (
+    "product_size_id" SERIAL NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "size_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductSize_pkey" PRIMARY KEY ("product_size_id")
 );
 
+CREATE TABLE "Color" (
+    "color_id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
 
-CREATE TABLE IF NOT EXISTS "Color" (
-    color_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    CONSTRAINT "Color_pkey" PRIMARY KEY ("color_id")
 );
 
-CREATE TABLE IF NOT EXISTS "ProductColor" (
-    product_color_id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,
-    color_id INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES "Product"(product_id),
-    FOREIGN KEY (color_id) REFERENCES "Color"(color_id)
+CREATE TABLE "ProductColor" (
+    "product_color_id" SERIAL NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "color_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductColor_pkey" PRIMARY KEY ("product_color_id")
 );
 
-CREATE TABLE IF NOT EXISTS "Order" (
-    order_id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,
-    user_id INT NOT NULL,
-    product_quantity INT NOT NULL,
-    date TIMESTAMPTZ(2) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES "Product"(product_id),
-    FOREIGN KEY (user_id) REFERENCES "Customer"(customer_id)
+CREATE TABLE "Order" (
+    "order_id" SERIAL NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "product_quantity" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("order_id")
 );
 
-CREATE TABLE IF NOT EXISTS "Shipment" (
-    shipment_id SERIAL PRIMARY KEY,
-    order_id INT NOT NULL,
-    total_amount INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES "Order"(order_id)
+CREATE TABLE "Shipment" (
+    "shipment_id" SERIAL NOT NULL,
+    "order_id" INTEGER NOT NULL,
+    "total_amount" INTEGER NOT NULL,
+
+    CONSTRAINT "Shipment_pkey" PRIMARY KEY ("shipment_id")
 );
 
-CREATE TABLE IF NOT EXISTS "Region" (
-    region_id SERIAL PRIMARY KEY,
-    name varchar(50) NOT NULL,
-    taxes INT NOT NULL
+CREATE TABLE "Region" (
+    "region_id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "taxes" INTEGER NOT NULL,
+
+    CONSTRAINT "Region_pkey" PRIMARY KEY ("region_id")
 );
 
+ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("product_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("category_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ProductSize" ADD CONSTRAINT "ProductSize_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("product_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ProductSize" ADD CONSTRAINT "ProductSize_size_id_fkey" FOREIGN KEY ("size_id") REFERENCES "Size"("size_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ProductColor" ADD CONSTRAINT "ProductColor_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("product_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ProductColor" ADD CONSTRAINT "ProductColor_color_id_fkey" FOREIGN KEY ("color_id") REFERENCES "Color"("color_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Order" ADD CONSTRAINT "Order_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("product_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Order" ADD CONSTRAINT "Order_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Customer"("customer_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Shipment" ADD CONSTRAINT "Shipment_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "Order"("order_id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ app/api/category/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import {prisma} from "@/lib/prisma";
+import {NextApiRequest, NextApiResponse} from "next";
+
+export async function GET() {
+  const category = await prisma.category.findMany();
+  return NextResponse.json(category);
+}
+
+export async function POST(request: { json: () => any; }) {
+  const data = await request.json();
+  await prisma.category.create({
+    data: {
+      name: data.name,
+      description: data.description
+    }
+  });
+
+  return NextResponse.json("Successful category creation");
+}
+
+export async function DELETE() {
+  await prisma.category.deleteMany();
+  return NextResponse.json("Successful All items of Category");
+}
