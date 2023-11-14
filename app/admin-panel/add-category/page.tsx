27512c9626befa74c14a316efa8b9d6daad017ab
AndrePzDn <andreperezduran@gmail.com>
2023-11-14 00:09:55 -0400

    feat: improve database model design and improve product crud

--- app/admin-panel/add-category/page.tsx
+++ app/admin-panel/add-category/page.tsx
@@ -1,14 +1,19 @@
 import Button from "@/app/components/Button";
 import InputWithTItle from "@/app/components/InputWithTitle";
+import Footer from "@/app/components/footer";
+import Link from "next/link";
 
 export default function AddCategory() {
   return (
     <div className="p-10 ml-10">
       <h1 className="font-bold text-4xl">ADD CATEGORY</h1>
-      <div className="pt-10 px-28">
+      <div className="pt-10 px-10 md:px-28">
         <InputWithTItle title='Category Name' />
-        <InputWithTItle title='Description' className="h-36" />
-        <div className="flex justify-end pt-20">
+        <InputWithTItle title='Description' />
+        <div className="flex justify-between pt-20">
+          <Link href='/admin-panel/category-list'>
+            <Button text='Back' className='text-xl px-10 py-3 rounded-sm' />
+          </Link>
           <Button text='Add' type='submit' className='text-xl px-10 py-3 rounded-sm'/>
         </div>
       </div>
--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
+import { createProductSchema } from './product.schema';
 
 export async function GET() {
   const products = await prisma.product.findMany({
@@ -10,35 +10,62 @@ export async function GET() {
           category: true,
         },
       },
-      ProductSize: {
+      Attribute: {
         include: {
           size: true,
-        },
-      },
-      ProductColor: {
-        include: {
           color: true,
         },
       },
     },
   });
 
+
   return NextResponse.json(products);
 }
 
 export async function POST(request: { json: () => any; }) {
-  const data = await request.json();
-  await prisma.product.create({
-    data: {
-      name: data.name,
-      description: data.description,
-      price: data.price,
-      image: data.image,
-      quantity: data.quantity
-    }
-  });
+  try {
+    const data = await request.json();
 
-  return NextResponse.json("Successful product creation");
+    createProductSchema.parse(data);
+
+    const createdProduct = await prisma.product.create({
+      data: {
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        image: data.image,
+        ProductCategory: {
+          create: {
+            category: {
+              connect: {
+                category_id: data.ProductCategory[0].category_id,
+              },
+            },
+          },
+        },
+        Attribute: {
+          create: {
+            size: {
+              connect: {
+                size_id: data.Attribute[0].size_id,
+              },
+            },
+            color: {
+              connect: {
+                color_id: data.Attribute[0].color_id,
+              },
+            },
+            quantity: data.Attribute[0].quantity,
+          },
+        },
+      },
+    });
+    return NextResponse.json(createdProduct);
+  } catch (error) {
+    console.error("Error", error)
+    return NextResponse.json({ error: (error as any).errors }, { status: 401 });
+  }
 }
 
 export async function DELETE() {
