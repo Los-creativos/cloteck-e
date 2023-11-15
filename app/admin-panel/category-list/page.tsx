232afd76c88570501e38f96bf0badc550cf26365
AndrePzDn <andreperezduran@gmail.com>
2023-11-14 23:15:02 -0400

    feat: improve admin panel list category ui

--- app/admin-panel/category-list/page.tsx
+++ app/admin-panel/category-list/page.tsx
@@ -1,22 +1,18 @@
 import Button from '@/app/components/Button'
-import CategoryDisplay from '@/app/components/CategoryDisplay'
 import CategoryLayout from '@/app/components/CategoryLayout'
-import Sidebar from '@/app/components/Sidebar'
 import Link from 'next/link'
 
 export default function CategoryList () {
   return (
     <div>
-      <Sidebar />
-      <div className='pl-72 pt-5 pr-10'>
-        <div className='flex flex-row'>
-          <h1 className='font-bold text-3xl'>CATEGORY LIST</h1>
+      <div className='p-10'>
+        <div className='flex flex-row items-center'>
+          <h1 className='font-bold md:text-3xl sm:text-xl text-lg'>CATEGORY LIST</h1>
           <Link href='/admin-panel/add-category'>
-            <Button text='+' className='ml-5 bg-slate-500 rounded-full' />
+            <Button text='+' className='ml-5 md:text-2xl  bg-slate-500 rounded-full' />
           </Link>
         </div>
         <div className='w-full mt-10'>
-          <CategoryDisplay />
           <CategoryLayout />
         </div>
       </div>
--- app/components/CategoryDisplay.tsx
+++ app/components/CategoryDisplay.tsx
@@ -12,11 +12,17 @@ export default function CategoryDisplay ({
   Remove?: String | ReactNode;
 }) {
   return (
-    <div className='grid grid-cols-7 gap-4 mx-auto text-center font-semibold text-lg'>
-      <div className='flex col-span-1 justify-center items-center'>{CategoryName}</div>
-      <div className='flex col-span-4 justify-center items-center'>{Description}</div>
-      <div className='flex col-span-1 justify-center items-center'>{Edit}</div>
-      <div className='flex col-span-1 justify-center items-center'>{Remove}</div>
+    <div className='grid lg:grid-cols-7 shadow-inner lg:grid-rows-1 grid-cols-2 lg:gap-4 text-center items-center text-lg bg-zinc-200 bg-opacity-60 rounded-xl lg:mb-3 mb-5  py-4 lg:p-0 p-2'>
+      <div className='lg:hidden flex font-semibold lg:col-span-1 col-span-2 justify-center'>Category Name</div>
+      <div className='flex lg:col-span-1 col-span-2 justify-center'>{CategoryName}</div>
+      <div className='lg:hidden border-t border-stone-300 w-full col-span-2 my-4' />
+      <div className='lg:hidden flex font-semibold lg:col-span-4 col-span-2 justify-center'>Description</div>
+      <div className='flex lg:col-span-4 col-span-2 justify-center'>{Description}</div>
+      <div className='lg:hidden border-t border-stone-300 w-full col-span-2 my-4' />
+      <div className='lg:hidden flex font-semibold col-span-1 justify-center'>Edit</div>
+      <div className='lg:hidden flex font-semibold col-span-1 justify-center'>Remove</div>
+      <div className='flex col-span-1 justify-center'>{Edit}</div>
+      <div className='flex col-span-1 justify-center'>{Remove}</div>
     </div>
   )
 }
