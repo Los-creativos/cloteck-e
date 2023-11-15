1d43063158678599bc4470bb06720dc5c414a089
isaias-rojas <s_isaiasr@tutanota.com>
2023-11-15 17:05:25 -0400

    Revert "Merge pull request #14 from Los-creativos/hotfix/fixBuildCategoryApi"
    This reverts commit d611c3bd8e69bf73cdaf3b29850ac4d776aef252, reversing
    changes made to e6add8b4de9ceed659597c84fde408da82f9572b.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '../styles/globals.css'
-import Nadvar from './components/Navbar'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,10 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-      <Nadvar />
-        <main>{children}</main>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
