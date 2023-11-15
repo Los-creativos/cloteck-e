9fd3a72ec7c6eaa2bf28575bd2044825011488b0
isaias-rojas <s_isaiasr@tutanota.com>
2023-11-15 17:23:27 -0400

    feat: remove test from app

--- app/__test__/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import  Home  from '../page';
-
-describe('Home', () => {
-    it('renders the Home component', () => {
-      render(<Home />);
-      expect(screen.getByTestId('home-container')).toBeInTheDocument();
-    });
-  });
