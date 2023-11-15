5b65fc3b56c2fc1f4a85be01fd9d0b3b33354eca
JosuePrado <prado69508698@gmail.com>
2023-11-15 00:05:24 -0400

    Update data base model and prisma conection, improve the category validators

--- app/api/category/[id]/route.ts
+++ app/api/category/[id]/route.ts
@@ -1,22 +1,32 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+import { prisma } from '@/lib/prisma'
+import {updateCategoryValidator} from '@/app/api/category/category.schema'
+import {ZodError} from 'zod'
 
 export async function GET(
   req: NextRequest,
   {
     params
   }: {
-    params: { id: string };
+    params: { id: string }
   }
 ) {
-  const uniqueCategory = await prisma.category.findUnique({
-    where: {
-      category_id: parseInt(params.id)
-    }
-  })
+  try {
+    const uniqueCategory = await prisma.category.findUnique({
+      where: {
+        category_id: parseInt(params.id)
+      }
+    })
 
-  return NextResponse.json(uniqueCategory)
+    return NextResponse.json(uniqueCategory)
+  } catch (error) {
+    console.error('Error', error)
+    if (error instanceof ZodError) {
+      return NextResponse.json({ error: (error as any).errors }, { status: 401 })
+    }
+    return NextResponse.json({ error: (error as any).errors }, { status: 500 })
+  }
 }
 
 export async function DELETE (
@@ -24,7 +34,7 @@ export async function DELETE (
   {
     params
   }: {
-    params: { id: string };
+    params: { id: string }
   }
 ) {
   await prisma.category.delete({
@@ -41,16 +51,16 @@ export async function PUT(
   {
     params
   }: {
-    params: { id: string };
+    params: { id: string }
   }
 ) {
   const data = await req.json()
+  updateCategoryValidator.parse(data)
   await prisma.category.update({
     where: {
       category_id: parseInt(params.id)
     },
     data: {
-      name: data.name,
       description: data.description
     }
   })
