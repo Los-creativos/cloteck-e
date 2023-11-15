8477b2c1a1ef6699d246a983e2457567247cef66
AndrePzDn <andreperezduran@gmail.com>
2023-11-15 00:54:08 -0400

    feat: add category remove and add from frontend side

--- app/admin-panel/add-category/page.tsx
+++ app/admin-panel/add-category/page.tsx
@@ -1,21 +1,58 @@
+'use client'
+
+import { useState } from 'react'
 import Button from '@/app/components/Button'
 import InputWithTItle from '@/app/components/InputWithTitle'
 import Link from 'next/link'
 
 export default function AddCategory () {
+  const [name, setCategoryName] = useState('')
+  const [description, setDescription] = useState('')
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+
+    try {
+      await fetch('/api/category/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          description
+        })
+      })
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
   return (
-    <div className='p-10 ml-10'>
-      <h1 className='font-bold text-4xl'>ADD CATEGORY</h1>
-      <div className='pt-10 px-10 md:px-28'>
-        <InputWithTItle title='Category Name' />
-        <InputWithTItle title='Description' />
-        <div className='flex justify-between pt-20'>
-          <Link href='/admin-panel/category-list'>
-            <Button text='Back' className='text-xl px-10 py-3 rounded-sm' />
-          </Link>
-          <Button text='Add' type='submit' className='text-xl px-10 py-3 rounded-sm' />
+    <div className='p-10'>
+      <h1 className='font-bold lg:text-3xl md:text-2xl text-xl sm:text-left text-center'>ADD CATEGORY</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='pt-10 md:px-10 lg:px-28'>
+          <InputWithTItle
+            title='Category Name'
+            value={name}
+            onChange={(e: any) => setCategoryName(e.target.value)}
+            className='sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]'
+          />
+          <InputWithTItle
+            title='Description'
+            value={description}
+            onChange={(e: any) => setDescription(e.target.value)}
+            className='3xl:min-h-[57px] lg:min-h-[80px] md:min-h-[120px] sm:min-h-[120px] min-h-[230px] 3xl:max-h-[57px] lg:max-h-[80px] md:max-h-[120px] sm:max-h-[120px] max-h-[230px]'
+          />
+          <div className='flex justify-between pt-20'>
+            <Link href='/admin-panel/category-list'>
+              <Button text='Back' className='text-xl px-10 py-3 rounded-sm' />
+            </Link>
+            <Button text='Add' type='submit' className='text-xl px-10 py-3 rounded-sm' />
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
--- app/components/InputWithTitle.tsx
+++ app/components/InputWithTitle.tsx
@@ -1,24 +1,22 @@
 export default function InputWithTItle ({
   title,
   placeholder,
-  type,
   value,
   onChange,
   className
 }:{
   title: string,
   placeholder?: string,
-  type?: string,
   value?: string,
   onChange?: any
   className?: string
 }) {
   return (
     <div className='flex flex-col'>
-      <h2 className='font-semibold text-xl mb-5 mt-5'>
+      <h2 className='font-semibold lg:text-xl md:text-lg mb-5 mt-5'>
         {title}
       </h2>
-      <input placeholder={placeholder} className={`bg-slate-300 opacity-50 p-4 rounded-sm ${className}`} type={type} />
+      <textarea onChange={onChange} value={value} placeholder={placeholder} className={`bg-slate-300 opacity-50 p-4 rounded-sm hover:bg-slate-400 ${className}`} />
     </div>
   )
 }
