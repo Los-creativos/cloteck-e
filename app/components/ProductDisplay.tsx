53159fa8a2f187bb850bc5e7ec9af2cc28fb4f61
AndrePzDn <andreperezduran@gmail.com>
2023-11-14 23:15:02 -0400

    feat: improve admin panel list category ui

--- app/components/ProductDisplay.tsx
+++ app/components/ProductDisplay.tsx
@@ -18,22 +18,22 @@ export default function ProductDisplay ({
   Remove?: String | ReactNode;
 }) {
   return (
-    <div className='grid lg:grid-cols-7 shadow-inner lg:grid-rows-1 grid-cols-2 lg:gap-4 lg:text-center text-lg bg-zinc-200 bg-opacity-60 rounded-xl lg:mb-3 mb-5  py-4 lg:p-0 p-2'>
-      <div className='lg:hidden font-semibold lg:col-span-1 col-span-2 text-center justify-center items-center'>Product Name</div>
-      <div className='flex lg:col-span-1 col-span-2 text-center justify-center items-center'>{ProductName}</div>
+    <div className='grid lg:grid-cols-7 shadow-inner lg:grid-rows-1 grid-cols-2 lg:gap-4 text-center text-lg bg-zinc-200 bg-opacity-60 rounded-xl lg:mb-3 mb-5  py-4 lg:p-0 p-2'>
+      <div className='lg:hidden font-semibold lg:col-span-1 col-span-2 justify-center items-center'>Product Name</div>
+      <div className='flex lg:col-span-1 col-span-2 justify-center items-center'>{ProductName}</div>
       <div className='lg:hidden border-t border-stone-300 w-full col-span-2 my-4' />
-      <div className='lg:hidden font-semibold justify-center items-center text-center'>Size</div>
-      <div className='lg:hidden font-semibold justify-center items-center text-center'>Colors</div>
+      <div className='lg:hidden font-semibold justify-center items-center'>Size</div>
+      <div className='lg:hidden font-semibold justify-center items-center'>Colors</div>
       <div className='flex justify-center items-center'>{Size}</div>
       <div className='flex justify-center items-center'>{Color}</div>
       <div className='lg:hidden border-t border-stone-300 w-full col-span-2 my-4' />
-      <div className='lg:hidden font-semibold justify-center items-center text-center'>Price</div>
-      <div className='lg:hidden font-semibold justify-center items-center text-center'>Category</div>
+      <div className='lg:hidden font-semibold justify-center items-center'>Price</div>
+      <div className='lg:hidden font-semibold justify-center items-center'>Category</div>
       <div className='flex justify-center items-center'>{Price}</div>
       <div className='flex justify-center items-center'>{Category}</div>
       <div className='lg:hidden border-t border-stone-300 w-full col-span-2 my-4' />
-      <div className='lg:hidden font-semibold justify-center items-center text-center'>Edit</div>
-      <div className='lg:hidden font-semibold justify-center items-center text-center'>Remove</div>
+      <div className='lg:hidden font-semibold justify-center items-center'>Edit</div>
+      <div className='lg:hidden font-semibold justify-center items-center'>Remove</div>
       <div className='flex justify-center items-center'>{Edit}</div>
       <div className='flex justify-center items-center'>{Remove}</div>
     </div>
