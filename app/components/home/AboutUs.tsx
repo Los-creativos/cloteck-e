85e29d5cd580482e01e45ed207b828d205beb9f7
isaias-rojas <s_isaiasr@tutanota.com>
2023-11-21 13:25:32 -0400

    feat: improve responsiviness on navbar

--- app/components/home/AboutUs.tsx
+++ app/components/home/AboutUs.tsx
@@ -1,8 +1,8 @@
 export default function AboutUs() {
     return (
-      <div className="container mx-auto px-4 my-2 md:my-8">
-        <div className="content my-8">
-          <p className="leading-relaxed font-bold text-left text-3xl sm:text-4xl md:text-5xl">
+      <div className="mx-6 md:my-6 sm:my-20">
+        <div className="my-36">
+          <p className="leading-relaxed font-bold text-left text-1xl sm:text-3xl md:text-3xl">
             Clotheck stands at the intersection of local Bolivian culture and global fashion trends, offering an unparalleled shopping experience for those who dare to stand out. With every stitch and seam, we infuse the spirit of Bolivian heritage into contemporary styles that resonate worldwide.
           </p>
         </div>
--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -6,41 +6,158 @@ const links = ['MEN', 'WOMEN', 'UNISEX', 'COLLECTIONS'];
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-      <nav className="bg-white p-4 flex flex-wrap items-center justify-between">
-        <div className="text-black text-3xl font-semibold mx-4">CLOTECK</div>
-        {/* Hamburger Icon */}
-        <div className="sm:hidden mx-4">
-          <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+    <>
+      <nav className="bg-white px-4 py-2 flex items-center justify-center w-full relative">
+        <button className="md:hidden absolute left-4 top-4 z-50" onClick={toggleMenu}>
+          {isMenuOpen ? (
             <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
-        {/* Navigation Links */}
-        <div className={`w-full sm:flex sm:items-center sm:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
-          <ul className="flex flex-col sm:flex-row gap-4 mt-4 sm:mt-0 sm:gap-6 list-none font-semibold">
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+          </svg>
+          )}
+        </button>
+
+        <div className="text-black text-3xl font-semibold z-10">CLOTECK</div>
+
+        <div className="hidden md:flex justify-between items-center absolute inset-0">
+          <ul className="flex sm:gap-3  gap-6  list-none font-semibold ml-4">
             {links.map((link) => (
-              <li key={link} className="hover:underline underline-offset-4 transition duration-300 ease-in-out">
-                <a href="#" className="text-black no-underline px-2">{link}</a>
+              <li key={link} className="underline-offset-4 text-xs transition duration-300 ease-in-out ">
+                <a href="#" className="text-black no-underline">{link}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-4 mr-4">
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            className='w-5 h-5'
+          >
+            {/* Icono de búsqueda */}
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            className='w-5 h-5'
+          >
+            {/* Icono de usuario */}
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            className='w-5 h-5'
+          >
+            {/* Icono de carrito de compras */}
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+            />
+          </svg>
+          </div>
+        </div>
+
+        {/* Mobile: Icons on the right */}
+        <div className="md:hidden absolute right-4 top-4 z-50 flex gap-4">
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            className='w-4 h-4'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            className='w-4 h-4'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            className='w-4 h-4'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+            />
+          </svg>
+        </div>
+
+        <div className={`fixed inset-y-0 left-0 transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out bg-white w-64 z-40 shadow-lg md:hidden`}>
+          <ul className="pt-24">
+            {links.map((link) => (
+              <li key={link} className="text-black px-6 py-2 hover:bg-gray-100">
+                {link}
               </li>
             ))}
           </ul>
         </div>
+
+        {isMenuOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 z-30" onClick={toggleMenu}></div>
+        )}
       </nav>
-      <div className="w-screen border-t" />
-    </div>
+    </>
   );
 }
