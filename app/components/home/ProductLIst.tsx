eaa9148fbcae50be97bd01a6e8dc5f43f1b013a9
isaias-rojas <s_isaiasr@tutanota.com>
2023-11-21 00:22:37 -0400

    feat: add padding in grid

--- app/components/home/ProductLIst.tsx
+++ app/components/home/ProductLIst.tsx
@@ -41,7 +41,7 @@ export default function ProductList() {
 
   return (
     <div className='flex items-center justify-center mb-2'>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-4 md:mb-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-20 mb-4 md:mb-4">
       {productsToShow.map((product) => (
         <ProductCard key={product.product_id} product={product} />
       ))}
