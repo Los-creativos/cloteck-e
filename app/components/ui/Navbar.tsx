897c82b6f1b4bfe30a1cd649b3c6d186e0cf90fa
AndrePzDn <andreperezduran@gmail.com>
2023-11-22 09:31:18 -0400

    refactor: update navbar

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -1,24 +1,62 @@
-const links = ['MEN', 'WOMEN', 'UNISEX', 'NEWS']
-export default function Nadvar () {
+'use client'
+import React, { useState } from 'react';
+
+const links = ['MEN', 'WOMEN', 'UNISEX', 'COLLECTIONS'];
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-      <nav className='bg-white flex justify-between items-center h-20 p-4'>
-        <p className='text-black text-3xl font-semibold m-4'>CLOTECK</p>
-        <ul className='flex gap-6 list-none font-semibold ml-4'>
-          {links.map((link) => (
-            <li key={link} className='pl-2'>
-              <a href='#' className='text-black'>{link}</a>
-            </li>
-          ))}
-        </ul>
-        <div className='flex gap-6 ml-auto'>
+    <>
+      <nav className="bg-white px-4 py-2 flex items-center justify-center w-full relative sticky top-0 z-50">
+        <button className="md:hidden absolute left-4 top-4 z-50" onClick={toggleMenu}>
+          {isMenuOpen ? (
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+          </svg>
+          )}
+        </button>
+
+        <div className="text-black text-3xl font-semibold z-10">CLOTECK</div>
+
+        <div className="hidden md:flex justify-between items-center absolute inset-0">
+          <ul className="flex md:gap-6 gap-10 list-none font-semibold ml-4">
+            {links.map((link) => (
+              <li key={link} className="underline-offset-4 sm:text-xs transition duration-300 ease-in-out ">
+                <a href="#" className="text-black no-underline">{link}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-4 mr-4">
           <svg
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
             viewBox='0 0 24 24'
             strokeWidth='2'
             stroke='currentColor'
-            className='w-7 h-7'
+            className='w-5 h-5'
           >
             {/* Icono de búsqueda */}
             <path
@@ -33,9 +71,8 @@ export default function Nadvar () {
             viewBox='0 0 24 24'
             strokeWidth='2'
             stroke='currentColor'
-            className='w-7 h-7'
+            className='w-5 h-5'
           >
-            {/* Icono de usuario */}
             <path
               strokeLinecap='round'
               strokeLinejoin='round'
@@ -48,13 +85,30 @@ export default function Nadvar () {
             viewBox='0 0 24 24'
             strokeWidth='2'
             stroke='currentColor'
-            className='w-7 h-7'
+            className='w-5 h-5'
           >
-            {/* Icono de carrito de compras */}
             <path
               strokeLinecap='round'
               strokeLinejoin='round'
               d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+            />
+          </svg>
+          </div>
+        </div>
+
+        <div className="md:hidden absolute right-4 top-4 z-50 flex gap-4">
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            className='w-4 h-4'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
             />
           </svg>
           <svg
@@ -63,19 +117,44 @@ export default function Nadvar () {
             viewBox='0 0 24 24'
             strokeWidth='2'
             stroke='currentColor'
-            className='w-7 h-7'
+            className='w-4 h-4'
           >
-            {/* Icono de configuraciones (settings) */}
             <path
               strokeLinecap='round'
               strokeLinejoin='round'
-              d='M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
+              d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='2'
+            stroke='currentColor'
+            className='w-4 h-4'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
             />
           </svg>
         </div>
 
+        <div className={`fixed inset-y-0 left-0 transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out bg-white w-64 z-40 shadow-lg md:hidden`}>
+          <ul className="pt-24">
+            {links.map((link) => (
+              <li key={link} className="text-black px-6 py-2 hover:bg-gray-100">
+                {link}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {isMenuOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 z-30" onClick={toggleMenu}></div>
+        )}
       </nav>
-      <div className='w-screen border-t' />
-    </div>
-  )
+    </>
+  );
 }
