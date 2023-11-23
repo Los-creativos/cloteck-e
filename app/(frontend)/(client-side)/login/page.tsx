08a4f552ca969ebbd8d5acde4eeb5ddf4582cb31
AndrePzDn <andreperezduran@gmail.com>
2023-11-23 10:05:35 -0400

    feat: add ui for login page

---
+++ app/(frontend)/(client-side)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/app/components/login/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <section className='flex items-center justify-center'>
+      <LoginForm />
+    </section>
+  )
+}
--- app/components/admin/InputWithTitle.tsx
+++ app/components/admin/InputWithTitle.tsx
@@ -5,7 +5,9 @@ export default function InputWithTItle ({
   onChange,
   className,
   generalClassname,
-  readOnly = false
+  readOnly = false,
+  textarea = true,
+  type
 }:{
   title: string,
   placeholder?: string,
@@ -13,17 +15,33 @@ export default function InputWithTItle ({
   onChange?: any,
   className?: string,
   generalClassname? : string,
-  readOnly?: boolean
+  readOnly?: boolean,
+  textarea?: boolean,
+  type?: "text" | "password" | "email" | "number" | "date" | "time" | "datetime-local" | "month" | "week" | "tel" | "url" | "search" | undefined
 }) {
-  return (
-    <div className={`flex flex-col ${generalClassname}`}>
-      <h2 className='font-semibold lg:text-xl md:text-lg w-full mb-5 mt-5'>
-        {title}
-      </h2>
-      {readOnly
-        ? <textarea readOnly value={value} placeholder={placeholder} className={`bg-slate-300 bg-opacity-50 p-4 rounded-sm hover:bg-slate-400 hover:bg-opacity-50 resize-none ${className}`} />
-        : <textarea onChange={onChange} value={value} placeholder={placeholder} className={`bg-slate-300 bg-opacity-50 p-4 rounded-sm hover:bg-slate-400 hover:bg-opacity-50 resize-none ${className}`} />
-      }
-    </div>
-  )
+  if (textarea) {
+    return (
+      <div className={`flex flex-col ${generalClassname}`}>
+        <h2 className='font-semibold lg:text-xl md:text-lg w-full mb-5 mt-5'>
+          {title}
+        </h2>
+        {readOnly
+          ? <textarea readOnly value={value} placeholder={placeholder} className={`bg-slate-300 bg-opacity-50 p-4 rounded-sm hover:bg-slate-400 hover:bg-opacity-50 resize-none ${className}`} />
+          : <textarea onChange={onChange} value={value} placeholder={placeholder} className={`bg-slate-300 bg-opacity-50 p-4 rounded-sm hover:bg-slate-400 hover:bg-opacity-50 resize-none ${className}`} />
+        }
+      </div>
+    )
+  } else {
+    return (
+      <div className={`flex flex-col ${generalClassname}`}>
+        <h2 className='font-semibold lg:text-xl md:text-lg w-full mb-5 mt-5'>
+          {title}
+        </h2>
+        {readOnly
+          ? <input type={type} readOnly value={value} placeholder={placeholder} className={`bg-slate-300 bg-opacity-50 p-4 rounded-sm hover:bg-slate-400 hover:bg-opacity-50 ${className}`} />
+          : <input type={type} onChange={onChange} value={value} placeholder={placeholder} className={`bg-slate-300 bg-opacity-50 p-4 rounded-sm hover:bg-slate-400 hover:bg-opacity-50 ${className}`} />
+        }
+      </div>
+    )
+  }
 }
