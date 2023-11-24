8e1feacc5bd6c6f85a09588384681611a0007440
AndrePzDn <andreperezduran@gmail.com>
2023-11-23 22:11:02 -0400

    fix: fix navbar links do not have functionallity

---
+++ app/(frontend)/(client-side)/collections/page.tsx
@@ -0,0 +1,7 @@
+export default function CollectionPage () {
+  return (
+    <section>
+      <h1>This is the Collections Page</h1>
+    </section>
+  )
+}
---
+++ app/(frontend)/(client-side)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/ui/Navbar";
+import Footer from "@/app/components/ui/footer";
+import { ReactNode } from "react";
+
+export default function ClientLayout ({children}: {children: ReactNode}) {
+  return (
+    <div className='flex flex-col min-h-screen justify-between'>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  )
+}
