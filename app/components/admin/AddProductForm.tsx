c552c770d2ad532d2763c6a8d49ae169a0802f3a
AndrePzDn <andreperezduran@gmail.com>
2023-11-27 00:25:51 -0400

    fix: fix improve error handling in add product

--- app/components/admin/AddProductForm.tsx
+++ app/components/admin/AddProductForm.tsx
@@ -1,55 +1,70 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import InputWithTItle from './InputWithTitle'
-import Button from '@/app/components/ui/Button'
-import LinkButton from '@/app/components/ui/LinkButton'
-import AddProductCheckBoxDropDown from './AddProductCheckboxDropDown'
-import Dropdown from '../ui/DropDown'
-import AttributeDisplay from './AttributeDisplay'
-import { createAttributeSchema } from '@/app/(backend)/api/attribute/attribute.schema'
+import { useState } from "react";
+import InputWithTItle from "./InputWithTitle";
+import Button from "@/app/components/ui/Button";
+import LinkButton from "@/app/components/ui/LinkButton";
+import AddProductCheckBoxDropDown from "./AddProductCheckboxDropDown";
+import Dropdown from "../ui/DropDown";
+import AttributeDisplay from "./AttributeDisplay";
+import { createAttributeSchema } from "@/app/(backend)/api/attribute/attribute.schema";
+import { ZodError } from "zod";
+import { createProductSchema } from "@/app/(backend)/api/products/product.schema";
+import toast, { Toaster } from "react-hot-toast";
 
 export interface Attribute {
-  size: string
-  color: string
-  quantity: number
-  image: string
+  size: string;
+  color: string;
+  quantity: number;
+  image: string;
 }
 
 export default function AddProductForm() {
-  const [name, setProductName] = useState('')
-  const [price, setPrice] = useState(0)
-  const [description, setDescription] = useState('')
-  const [size, setSize] = useState('')
-  const [color, setColor] = useState('')
-  const [quantity, setQuantity] = useState(0)
-  const [attributes, setAttributes] = useState<Attribute[]>([])
-  const [selectedCategories, setSelectedCategories] = useState<number[]>([])
-  const [imageFile, setImageFile] = useState(new FormData())
+  const [name, setProductName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState("");
+  const [size, setSize] = useState("");
+  const [color, setColor] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [attributes, setAttributes] = useState<Attribute[]>([]);
+  const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
+  const [imageFile, setImageFile] = useState(new FormData());
 
   const handleCategoriesChange = (categories: number[]) => {
-    setSelectedCategories(categories)
-  }
+    setSelectedCategories(categories);
+  };
+
+  const dismissToasts = () => {
+    toast.dismiss();
+  };
 
   const handleUploadImage = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const file = event.target.files?.[0]
+    const file = event.target.files?.[0];
     if (file) {
-      const formData = new FormData()
-      formData.append('file', file)
-      formData.append('upload_preset', 'ecommerce-cloteck')
-      setImageFile(formData)
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "ecommerce-cloteck");
+      setImageFile(formData);
     }
-  }
+  };
 
   const handleAddAttribute = async () => {
-    if (size !== '' && color !== '' && quantity !== 0 && imageFile !== new FormData()) {
+    if (
+      size !== "" &&
+      color !== "" &&
+      quantity !== 0 &&
+      imageFile !== new FormData()
+    ) {
       try {
-        const imageUpdatedData = await fetch('https://api.cloudinary.com/v1_1/dnfdovmde/image/upload', {
-          method: 'POST',
-          body: imageFile,
-        });
+        const imageUpdatedData = await fetch(
+          "https://api.cloudinary.com/v1_1/dnfdovmde/image/upload",
+          {
+            method: "POST",
+            body: imageFile,
+          }
+        );
         const uploadedImageData = await imageUpdatedData.json();
         const attribute = {
           size,
@@ -57,21 +72,26 @@ export default function AddProductForm() {
           quantity,
           image: uploadedImageData.secure_url,
         };
-        createAttributeSchema.parse(attribute)
+        createAttributeSchema.parse(attribute);
         setAttributes([...attributes, attribute]);
       } catch (error) {
-        console.error('Error uploading image:', error);
-        alert('Error adding attribute');
+        if (error instanceof ZodError) {
+          dismissToasts();
+          `${Object.values(error.issues).map((issue) => {
+            toast.error(issue.path + ": " + issue.message, {
+              position: "top-left",
+            });
+          })}`;
+        }
       }
     } else {
-      alert('Please fill all the fields');
+      dismissToasts();
+      toast.error("Please fill all the fields", { position: "top-left" });
     }
   };
-  
 
   const handleSubmit = async (e: any) => {
-    e.preventDefault()
-
+    e.preventDefault();
     const productData = {
       name,
       description,
@@ -85,41 +105,49 @@ export default function AddProductForm() {
         quantity: attribute.quantity,
         image: attribute.image,
       })),
-    }
+    };
 
     try {
-      const response = await fetch('/api/products/', {
-        method: 'POST',
+      createProductSchema.parse(productData);
+      const response = await fetch("/api/products/", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(productData),
-      })
+      });
       response.ok
-        ? alert('Product added successfully')
-        : alert('Product not added')
+        ? alert("Product added successfully")
+        : alert("Product not added");
     } catch (error) {
-      console.error('Error:', error)
+      if (error instanceof ZodError) {
+        dismissToasts();
+        `${Object.values(error.issues).map((issue) => {
+          toast.error(issue.path + ": " + issue.message, {
+            position: "top-left",
+          });
+        })}`;
+      }
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className='pt-10 md:px-10 lg:px-28'>
-        <div className='grid grid-rows-3 grid-cols-1 sm:grid-cols-3 sm:grid-rows-1 sm:gap-x-4'>
+      <div className="pt-10 md:px-10 lg:px-28">
+        <div className="grid grid-rows-3 grid-cols-1 sm:grid-cols-3 sm:grid-rows-1 sm:gap-x-4">
           <InputWithTItle
-            title='Product Name'
-            placeholder='Ej. T-shirt ALC'
+            title="Product Name"
+            placeholder="Ej. T-shirt ALC"
             value={name}
             onChange={(e: any) => setProductName(e.target.value)}
-            className='sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]'
+            className="sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]"
           />
           <InputWithTItle
-            title='Price'
-            placeholder='Ej. 163.00'
-            value={price === 0 ? '' : price.toString()}
+            title="Price"
+            placeholder="Ej. 163.00"
+            value={price === 0 ? "" : price.toString()}
             onChange={(e: any) => setPrice(parseFloat(e.target.value) || 0)}
-            className='sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]'
+            className="sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]"
           />
           <AddProductCheckBoxDropDown
             selectedCategories={selectedCategories}
@@ -127,63 +155,63 @@ export default function AddProductForm() {
           />
         </div>
         <InputWithTItle
-          title='Description'
-          placeholder='Ej. T-shirt ALC is the new mode of T-shirts and now is available in Cloteck'
+          title="Description"
+          placeholder="Ej. T-shirt ALC is the new mode of T-shirts and now is available in Cloteck"
           value={description}
           onChange={(e: any) => setDescription(e.target.value)}
-          className='3xl:min-h-[57px] lg:min-h-[80px] md:min-h-[120px] sm:min-h-[120px] min-h-[230px] 3xl:max-h-[57px] lg:max-h-[80px] md:max-h-[120px] sm:max-h-[120px] max-h-[230px]'
+          className="3xl:min-h-[57px] lg:min-h-[80px] md:min-h-[120px] sm:min-h-[120px] min-h-[230px] 3xl:max-h-[57px] lg:max-h-[80px] md:max-h-[120px] sm:max-h-[120px] max-h-[230px]"
         />
-        <div className='grid md:grid-cols-4 grid-cols-2 md:grid-rows-1 grid-rows-2 gap-x-4'>
+        <div className="grid md:grid-cols-4 grid-cols-2 md:grid-rows-1 grid-rows-2 gap-x-4">
           <InputWithTItle
-            title='Size'
-            placeholder='Ej. XL'
+            title="Size"
+            placeholder="Ej. XL"
             value={size}
             onChange={(e: any) => setSize(e.target.value)}
-            className='sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]'
+            className="sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]"
           />
           <InputWithTItle
-            title='Color'
-            placeholder='Ej. #FFFFF'
+            title="Color"
+            placeholder="Ej. #FFFFF"
             value={color}
             onChange={(e: any) => setColor(e.target.value)}
-            className='sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]'
+            className="sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]"
           />
           <InputWithTItle
-            title='Quantity'
-            placeholder='Ej. 54'
-            value={quantity === 0 ? '' : quantity.toString()}
+            title="Quantity"
+            placeholder="Ej. 54"
+            value={quantity === 0 ? "" : quantity.toString()}
             onChange={(e: any) => setQuantity(parseInt(e.target.value) || 0)}
-            className='sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]'
+            className="sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px]"
           />
           <div>
-            <h2 className='font-semibold lg:text-xl md:text-lg w-full mb-5 mt-5'>
+            <h2 className="font-semibold lg:text-xl md:text-lg w-full mb-5 mt-5">
               Image
             </h2>
             <input
-              className='sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px] p-4 bg-slate-300 bg-opacity-50 w-full text-sm text-gray-900 border rounded-lg hover:bg-slate-400 hover:bg-opacity-50 cursor-pointer'
-              type='file'
-              accept='image/png'
+              className="sm:min-h-[57px] min-h-[80px] sm:max-h-[57px] max-h-[80px] p-4 bg-slate-300 bg-opacity-50 w-full text-sm text-gray-900 border rounded-lg hover:bg-slate-400 hover:bg-opacity-50 cursor-pointer"
+              type="file"
+              accept="image/png"
               onChange={handleUploadImage}
             />
           </div>
           {attributes.length === 0 ? (
             <Button
               onClick={handleAddAttribute}
-              text='Add Attribute +'
-              className='rounded-full md:col-span-4 col-span-2 mt-5 bg-slate-400 hover:bg-slate-500'
-              type='button'
+              text="Add Attribute +"
+              className="rounded-full md:col-span-4 col-span-2 mt-5 bg-slate-400 hover:bg-slate-500"
+              type="button"
             />
           ) : (
-            <div className='md:col-span-4 col-span-2'>
+            <div className="md:col-span-4 col-span-2">
               <Button
                 onClick={handleAddAttribute}
-                text='Add Attribute +'
-                className='rounded-t-3xl w-full mt-5 bg-slate-400 hover:bg-slate-500'
-                type='button'
+                text="Add Attribute +"
+                className="rounded-t-3xl w-full mt-5 bg-slate-400 hover:bg-slate-500"
+                type="button"
               />
               <Dropdown
-                classnameButton='w-full p-3 rounded-b-3xl text-center'
-                text='See your actual attributes'
+                classnameButton="w-full p-3 rounded-b-3xl text-center"
+                text="See your actual attributes"
               >
                 {attributes.map((attribute, i) => {
                   return (
@@ -194,25 +222,26 @@ export default function AddProductForm() {
                       quantity={attribute.quantity}
                       size={attribute.size}
                     />
-                  )
+                  );
                 })}
               </Dropdown>
             </div>
           )}
         </div>
-        <div className='flex justify-between pt-20'>
+        <div className="flex justify-between pt-20">
           <LinkButton
-            text='Back'
-            className='text-xl px-10 py-3 rounded-sm'
-            href='/admin/product'
+            text="Back"
+            className="text-xl px-10 py-3 rounded-sm"
+            href="/admin/product"
           />
           <Button
-            text='Add'
-            type='submit'
-            className='text-xl px-10 py-3 rounded-sm'
+            text="Add"
+            type="submit"
+            className="text-xl px-10 py-3 rounded-sm"
           />
         </div>
       </div>
+      <Toaster reverseOrder/>
     </form>
-  )
+  );
 }
