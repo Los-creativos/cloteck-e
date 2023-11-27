4ef1252be3872c8d52e2248d1723f3b4eb44e514
AndrePzDn <andreperezduran@gmail.com>
2023-11-27 01:11:13 -0400

    fix: fix allowed special characters in category management

--- app/(backend)/api/category/category.schema.ts
+++ app/(backend)/api/category/category.schema.ts
@@ -1,22 +1,23 @@
 import { number, object, string } from 'zod'
 
+const NO_SPECIAL_CHARACTERS_REGEX = new RegExp('^[\w\d\s]+$')
+
 export const createCategoryValidator = object({
   name: string({
     required_error: "The name is required",
     invalid_type_error: "The name must be a string"
-  }).trim().min(5, 'Minimum of 5 characters required').max(50, 'Maximum characters is 50'),
+  }).trim().min(5, 'Minimum of 5 characters required').max(50, 'Maximum characters is 50').regex(NO_SPECIAL_CHARACTERS_REGEX, 'The name should not have special characters'),
   description: string({
     required_error: "The description is required",
     invalid_type_error: "The description must be a string"
-    
-  }).trim().min(20, 'Minimum of 20 characters required').max(200, 'Maximum characters is 200')
+  }).trim().min(20, 'Minimum of 20 characters required').max(200, 'Maximum characters is 200').regex(NO_SPECIAL_CHARACTERS_REGEX, 'The description should not have special characters')
 })
 
 export const updateCategoryValidator = object({
   description: string({
     required_error: "The description is required",
     invalid_type_error: "The description must be a string"
-  }).trim().min(20, 'Minimum of 20 characters required').max(200, 'Maximum characters is 200')
+  }).trim().min(20, 'Minimum of 20 characters required').max(200, 'Maximum characters is 200').regex(NO_SPECIAL_CHARACTERS_REGEX, 'The description should not have special characters')
 }).partial()
 
 export const filterQuery = object({
