7633741bb8cea72bc5d2706c9d05a4d3311bd634
gabitrujillo <136093915+gabitrujillo@users.noreply.github.com>
2023-11-27 22:54:14 -0400

    fix: Comflicts layout.tsx

--- app/(frontend)/(client-side)/layout.tsx
+++ app/(frontend)/(client-side)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function ClientLayout ({children}: {children: ReactNode}) {
   return (
-    <div className='flex flex-col min-h-screen justify-between'>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       {children}
       <Footer />
