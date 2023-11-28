f48de33e931bfdf26189f74728aa97e929eb737d
isaias-rojas <s_isaiasr@tutanota.com>
2023-11-28 11:28:16 -0400

    feat: add backend integration to our collection

--- app/(frontend)/(client-side)/collections/page.tsx
+++ app/(frontend)/(client-side)/collections/page.tsx
@@ -2,59 +2,48 @@ import Image from "next/image";
 import Link from "next/link";
 
 
-const CollectionPage = () => {
-    const collections = [
-        {
-          name: "Men",
-          description: "High-quality men's clothing, expertly designed to marry style with comfort. From elegantly tailored office wear to chic casual pieces perfect for a weekend getaway.",
-          url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_a_neatly_arranged_photo_of_mens_clothes_t-shirt_gray_hoodie__c5a4ebe4-ccb1-4d94-ae56-a1b33b4e71d9_1.png?v=1675456349",
-          slug: "men",
-        },
-        {
-          name: "Women",
-          description: "High-quality women's clothing, expertly designed to marry style with comfort. From elegantly tailored office wear to chic casual pieces perfect for a weekend getaway.",
-          url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_a_neatly_arranged_photo_of_womens_clothes_t-shirt_yoga_pants_1f41a422-4293-4b40-b986-8d514fb2041c_1.png?v=1675456116",
-          slug: "women",
-        },
-        {
-          name: "Unisex",
-          description: "High-quality unisex clothing, expertly designed to marry style with comfort. From elegantly tailored office wear to chic casual pieces perfect for a weekend getaway.",
-          url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_three_pairs_of_neatly_arranged_men_and_womens_running_shoes._f4121e54-2c8a-4ad2-b366-355c0cc4348d_7.png?v=1675467965",
-          slug: "unisex",
-        },
-        {
-          name: "Tops",
-          description: "Our Tops collection offers a range of premium, organic cotton shirts and jackets. Each piece is designed with a minimalist aesthetic, ensuring you stay stylish while feeling comfortable.",
-          url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_three_pairs_of_neatly_arranged_men_and_womens_running_shoes._f4121e54-2c8a-4ad2-b366-355c0cc4348d_6.png?v=1675467128",
-          slug: "tops",
-        },
-        {
-          name: "Bottoms",
-          description: "Our Bottoms collection offers a range of organic cotton pants and shorts. Experience the ultimate comfort and style, designed for both active and relaxed lifestyles.",
-          url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_three_pairs_of_neatly_arranged_men_and_womens_running_shoes._f4121e54-2c8a-4ad2-b366-355c0cc4348d_4.png?v=1675465297",
-          slug: "bottoms",
-        },
-        {
-          name: "Accessories",
-          description: "",
-          url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_black_sport_bag_containing_sport_accessories_one_towel_one_w_ef3e1828-4bde-4d7f-a6bf-e7abae4961e8.png?v=1675462987",
-          slug: "accessories",
-        },
-        {
-          name: "Featured",
-          description: "Discover the pinnacle of Liquid's athletic wear in our Featured collection. These pieces encapsulate our commitment to sustainable fashion, merging comfort, style, and eco-consciousness.",
-          url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/banner-2.png?v=1675462488",
-          slug: "featured",
-        },
-        {
-          name: "Shoes",
-          description: "Step into sustainable fashion with Liquid's Shoes collection. Crafted for comfort and style, our shoes are the perfect complement to your eco-conscious wardrobe.",
-          url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_three_pairs_of_neatly_arranged_men_and_womens_running_shoes._f4121e54-2c8a-4ad2-b366-355c0cc4348d_1.png?v=1675461870",
-          slug: "shoes",
-        },
-      ];
+async function getCollectionData() {
+  const res = await fetch("http://localhost:3000/api/category");
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+async function CollectionPage () {
+  const collections = await getCollectionData()
+  const imageURLs = [
+    {
+      category_id: 1,
+      url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_a_neatly_arranged_photo_of_mens_clothes_t-shirt_gray_hoodie__c5a4ebe4-ccb1-4d94-ae56-a1b33b4e71d9_1.png?v=1675456349",
+    },
+    {
+      category_id: 2,
+      url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_a_neatly_arranged_photo_of_womens_clothes_t-shirt_yoga_pants_1f41a422-4293-4b40-b986-8d514fb2041c_1.png?v=1675456116",
+    },
+    {
+      category_id: 3,
+      url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_three_pairs_of_neatly_arranged_men_and_womens_running_shoes._f4121e54-2c8a-4ad2-b366-355c0cc4348d_7.png?v=1675467965"
+    },
+    {
+      category_id: 4,
+      url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_three_pairs_of_neatly_arranged_men_and_womens_running_shoes._f4121e54-2c8a-4ad2-b366-355c0cc4348d_6.png?v=1675467128"
+    },
+    {
+      category_id: 5,
+      url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/collections/cd_three_pairs_of_neatly_arranged_men_and_womens_running_shoes._f4121e54-2c8a-4ad2-b366-355c0cc4348d_4.png?v=1675465297"
+    }
+  ]
+
+  collections.forEach((category) => {
+    const imageURL = imageURLs.find((image) => image.category_id === category.category_id); 
+    const slug = category.name.toLowerCase().replace(/\s/g, '-');
+    category.url = imageURL ? imageURL.url : ''; 
+    category.slug = slug;
+  });
       
-      
+  
     return (
     <>
       <h1 className="text-4xl font-bold text-center mt-10">Collections</h1>
